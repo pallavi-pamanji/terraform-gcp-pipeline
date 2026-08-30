d39d017edf0dd8fde10566d0bb0d9a32892173a9
Pallavi Pamanji <113966535+pallavi-pamanji@users.noreply.github.com>
2026-08-30 11:38:29 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "project_id" {
+  description = "Google Cloud project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "Google Cloud region"
+  type        = string
+  default     = "europe-west4"
+}
+
+variable "bucket_name" {
+  description = "Google Cloud Storage bucket name"
+  type        = string
+}
+
+variable "bucket_location" {
+  description = "Google Cloud Storage bucket location"
+  type        = string
+  default     = "US"
+}
